e099033944803a588533242cfd3e78e94a327533
Florent Roques <florent.open@gmail.com>
2020-05-19 00:57:11 +0200

    fix Uncaught ReferenceError: RevealMenu is not defined
    specifying window.RevealMenu instead of only RevealMenu to avoid "Uncaught ReferenceError: RevealMenu is not defined" error

--- toolbar.js
+++ toolbar.js
@@ -186,7 +186,7 @@ var RevealToolbar =
         if (captureMenu) {
           // handle async loading of plugins
           var id_menu = setInterval(function() {
-            if (RevealMenu && RevealMenu.isInit()) {
+            if (window.RevealMenu && RevealMenu.isInit()) {
               dom.menu = document.querySelector('div.slide-menu-button');
               if (dom.menu) {
                 console.log('Moving menu button');
